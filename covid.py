8e876a2d6875b069c3d1af9f12b62699317e2428
dairof7 <dairof7@gmail.com>
2020-05-09 16:14:14 -0500

    sintetizando

--- covid.py
+++ covid.py
@@ -6,24 +6,13 @@ def verify_spot(matrix, spot, pos=[]):
 	# 	return "Pared"
 	# if pos[1] - spot < 0 or pos[1] + spot > len(matrix):
 	# 	return "Pared"
-	for i in range(1, spot + 1):
-		if matrix[pos[0] - i][pos[1] - i] == 1:
-			return False
-		if matrix[pos[0] - i][pos[1]] == 1:
-			return False
-		if matrix[pos[0] - i][pos[1] + i] == 1:
-			return False
-		if matrix[pos[0]][pos[1] - i] == 1:
-			return False
-		if matrix[pos[0]][pos[1] + i] == 1:
-			return False
-		if matrix[pos[0] + i][pos[1] - i] == 1:
-			return False
-		if matrix[pos[0] + i][pos[1]] == 1:
-			return False
-		if matrix[pos[0] + i][pos[1] + i] == 1:
-			return False
-	return True
+    for i in range(-spot, spot + 1):
+        for j in range (-spot, spot + 1):
+            #print("<{},{}> = {}".format(i,j,matrix[pos[0] + i][pos[1] + j]))
+            if matrix[pos[0] + i][pos[1] + j] == 1:
+                print("Collision in: ({}, {})".format(pos[0] + i, pos[1] + i))
+                return False
+    return True
 
 def print_matrix(matrix):
     print("________________________________")
@@ -36,13 +25,13 @@ spots = [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 1, 0, 0, 1, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0],
 		[0, 1, 0, 1, 0, 0, 0, 0, 0],
 		[0, 0, 0, 0, 0, 0, 0, 0, 0]
 	]
-allowed_spots = 1
+allowed_spots = 2
 
 x1 = int(input("coordenada en x ini: "))
 y1 = int(input("coordenada en y ini: "))
@@ -51,10 +40,13 @@ x2 = int(input("coordenada en x fin: "))
 y2 = int(input("coordenada en y fin: "))
 spots[x1][y1] = "x"
 print_matrix(spots)
+if not verify_spot(spots, allowed_spots, [x1, y1]):
+    print("cant move")
+    exit ()
+
 if ((x1-x2 != 0) ^ (y1-y2 != 0)):
     # vertical move
     if (x1 - x2 > 0):
-        spots[x1][y1] = 0
         print("arriba")
         for i in range(abs(x1 - x2)):
             x1 -= 1
